de5d1a0416e15b0fcf25af8fe2947a36f539f13e
Trung Do <dttr278@gmail.com>
2023-08-28 17:57:06 +0700

    worker:main thread - return when error

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -79,7 +79,7 @@ async fn main() -> Result<()> {
 
 	let t1 = tokio::spawn(run_worker_1);
 	let t2: tokio::task::JoinHandle<_> = tokio::spawn(run_worker_2);
-	let (_, _) = (t1.await, t2.await);
+	let (_, _) = (t1.await?, t2.await?);
 
 	Ok(())
 }
