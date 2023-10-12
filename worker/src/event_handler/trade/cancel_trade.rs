f8c026d40c91b6ebcae299a5e999212231269d43
Trung Do <dttr278@gmail.com>
2023-10-12 14:45:17 +0700

    write uni test for trade services

--- worker/src/event_handler/trade/cancel_trade.rs
+++ worker/src/event_handler/trade/cancel_trade.rs
@@ -1,8 +1,4 @@
-use mongodb::bson::doc;
-use shared::{
-	constant::{EVENT_TRADE_CANCELLED, TRADE_STATUS_CANCELED},
-	models, BaseDocument, Trade,
-};
+use shared::constant::EVENT_TRADE_CANCELLED;
 pub use shared::{
 	constant::{TRADE_SET_AUCTION, TRADE_SET_WIST_LIST},
 	types::Result,
@@ -10,51 +6,25 @@ pub use shared::{
 
 use crate::{
 	gafi::{self},
-	services,
-	workers::{HandleParams, EventHandle},
+	services::trade_service,
+	types::CancelTradeParams,
+	workers::{EventHandle, HandleParams},
 };
 
 async fn on_trade_cancelled(params: HandleParams<'_>) -> Result<()> {
 	let event_parse = params.ev.as_event::<gafi::game::events::TradeCanceled>()?;
 	if let Some(ev) = event_parse {
-		let trade_db = params.db.collection::<Trade>(models::trade::Trade::name().as_str());
-		let query = doc! {
-		  "trade_id": ev.trade,
-		};
-		let update = doc! {
-		  "status": TRADE_STATUS_CANCELED,
-		};
-		let trade = trade_db.find_one(query.clone(), None).await?.unwrap();
-		trade_db.update_one(query.clone(), update, None).await?;
-
-		let mut nfts: Option<Vec<models::trade::Nft>> = None;
-		if trade.nft.is_some() {
-			nfts = Some(vec![trade.nft.unwrap()]);
-		} else if trade.bundle.is_some() {
-			nfts = trade.bundle;
-		} else if trade.maybe_required.is_some() {
-			nfts = trade.maybe_required;
-		};
-
-		let history = models::HistoryTx {
-			trade_id: Some(ev.trade.to_string()),
-			from: hex::encode(ev.who.0),
-			id: None,
-			amount: None,
-			block_height: params.block.height,
-			event: EVENT_TRADE_CANCELLED.to_string(),
-			event_index: params.ev.index(),
-			extrinsic_index: params.extrinsic_index.unwrap(),
-			nfts,
-			pool: None,
-			price: None,
-			to: None,
-			tx_hash: None,
-			value: None,
-			source: None,
-			trade_type: Some(trade.trade_type),
-		};
-		services::history_service::upsert(history, params.db).await?;
+		trade_service::cancel_trade(
+			CancelTradeParams {
+				block_height: params.block.height,
+				event_index: params.ev.index(),
+				extrinsic_index: params.extrinsic_index.unwrap(),
+				trade_id: ev.trade.to_string(),
+				who: hex::encode(ev.who.0),
+			},
+			params.db,
+		)
+		.await?;
 	}
 	Ok(())
 }
